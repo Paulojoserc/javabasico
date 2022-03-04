5b06ae8d8cc10946f2706d34f8c23771fc378407
Paulo Costa <pjrc83@gmail.com>
2022-03-04 17:28:29 -0300

    exer4

---
+++ src/javabasico/aula17/labs/Exer04.java
@@ -0,0 +1,19 @@
+package javabasico.aula17.labs;
+
+public class Exer04 {
+public static void main(String[] args) {
+	int popA = 80000;
+	int popB = 200000;
+	int cont = 0;
+	
+	while (popA < popB) {
+		popA += (popA /100) *3;
+		popB += (popB /100) *1.5; 
+		cont++;
+	}
+	
+	System.out.println("População A: " +popA);
+	System.out.println("População B: " +popB);
+	System.out.println("O numero de anos é "+ cont);
+}
+}
