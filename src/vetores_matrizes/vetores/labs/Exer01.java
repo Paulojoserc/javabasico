7e9c9934f7efbdc4b47a326feb78ccbadf8f4d63
Paulo Costa <pjrc83@gmail.com>
2022-03-15 17:53:46 -0300

    exer01

--- src/vetores_matrizes/vetores/labs/Exer01.java
+++ src/vetores_matrizes/vetores/labs/Exer01.java
@@ -3,13 +3,26 @@ package vetores_matrizes.vetores.labs;
 import java.util.Scanner;
 
 public class Exer01 {
-public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	
-	int[] vetorA = new int[5];
-	int[] vetorB = new int[5];
-	
-	
-	sc.close();
-}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] vetorA = new int[5];
+		int[] vetorB = new int[vetorA.length];
+
+		for (int i = 0; i < vetorA.length; i++) {
+
+			System.out.println("Entre com o valor da posição: " + i);
+			vetorA[i] = sc.nextInt();
+			
+			vetorB[i] = vetorA[i];
+		}
+		
+		System.out.println("Vetor A =");
+		for (int i=0; i<vetorA.length; i++) {
+			System.out.print(vetorA[i]+ " ");
+		}
+		System.out.println();
+
+		sc.close();
+	}
 }
